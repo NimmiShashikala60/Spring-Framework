1116daa1dd6f00a230f8da95e18becdd6ae2f71d
NimmiShashikala60 <nimmishashikala385@gmail.com>
2023-01-12 22:13:48 +0530

    new update

--- 01_Hello_Spring/src/lk/ijse/spring/AppInitializer.java
+++ 01_Hello_Spring/src/lk/ijse/spring/AppInitializer.java
@@ -1,12 +1,13 @@
 package lk.ijse.spring;
 
 import lk.ijse.spring.config.AppConfig;
+import lk.ijse.spring.pojo.Customer;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AppInitializer {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext ctx=new AnnotationConfigApplicationContext();
-        ctx.register(AppConfig.class);
+        ctx.register(Customer.class);
         ctx.refresh();
         ctx.close();
     }
--- 01_Hello_Spring/src/lk/ijse/spring/config/AppConfig.java
+++ 01_Hello_Spring/src/lk/ijse/spring/config/AppConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "lk.ijse.spring")
+@ComponentScan(basePackages = "lk.ijse.spring.config")
 public class AppConfig {
 
     public AppConfig(){
