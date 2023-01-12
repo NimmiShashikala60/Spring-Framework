6741d9bbef563ef8d00eda65c0e47c25b005ecd3
NimmiShashikala60 <nimmishashikala385@gmail.com>
2023-01-12 22:45:43 +0530

    new update

--- 01_Hello_Spring/src/lk/ijse/spring/AppInitializer.java
+++ 01_Hello_Spring/src/lk/ijse/spring/AppInitializer.java
@@ -2,13 +2,28 @@ package lk.ijse.spring;
 
 import lk.ijse.spring.config.AppConfig;
 import lk.ijse.spring.pojo.Customer;
+import lk.ijse.spring.pojo.DBConnection;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AppInitializer {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext ctx=new AnnotationConfigApplicationContext();
-        ctx.register(Customer.class);
+        ctx.register(AppConfig.class);
         ctx.refresh();
+
+        Customer c1= ctx.getBean(Customer.class);
+        System.out.println(c1);
+        Customer c2= ctx.getBean(Customer.class);
+        System.out.println(c2);
+        c1.getCustomerName();
+
+
+        DBConnection db1 = ctx.getBean(DBConnection.class);
+        DBConnection db2 = ctx.getBean(DBConnection.class);
+        System.out.println(db1);
+        System.out.println(db2);
+
+
         ctx.close();
     }
 }
--- 01_Hello_Spring/src/lk/ijse/spring/config/AppConfig.java
+++ 01_Hello_Spring/src/lk/ijse/spring/config/AppConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "lk.ijse.spring.config")
+@ComponentScan(basePackages = "lk.ijse.spring.pojo")
 public class AppConfig {
 
     public AppConfig(){
