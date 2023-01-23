4d4d2b8a808391d628b164f9440a713ea6a3754d
NimmiShashikala60 <nimmishashikala385@gmail.com>
2023-01-23 13:34:07 +0530

    new update

---
+++ 08_Sprimg_Enviroment/src/lk/ijse/spring/pojo/DBConnection.java
@@ -0,0 +1,24 @@
+package lk.ijse.spring.pojo;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DBConnection implements InitializingBean {
+    @Value("${os.name}")
+    private String myOSName;
+
+    @Value("${user}")
+    private String myUserName;
+
+    public DBConnection(){
+        System.out.println("DBConnection:Instantiated");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println(myOSName);
+        System.out.println(myUserName);
+    }
+}
