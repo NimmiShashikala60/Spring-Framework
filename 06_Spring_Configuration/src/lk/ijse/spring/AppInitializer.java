7178d522b949a15177e8cc2be65a721c807005d0
NimmiShashikala60 <nimmishashikala385@gmail.com>
2023-01-23 09:44:20 +0530

    new update

--- 06_Spring_Configuration/src/lk/ijse/spring/AppInitializer.java
+++ 06_Spring_Configuration/src/lk/ijse/spring/AppInitializer.java
@@ -9,8 +9,8 @@ public class AppInitializer {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext ctx=new AnnotationConfigApplicationContext();
         ctx.register(AppConfig.class);
-        ctx.register(ConfigOne.class);
-        ctx.register(ConfigTwo.class);
+//        ctx.register(ConfigOne.class);
+//        ctx.register(ConfigTwo.class);
         ctx.refresh();
         ctx.registerShutdownHook();
 
