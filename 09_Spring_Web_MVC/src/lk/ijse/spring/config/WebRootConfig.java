2f33d2ec2206e2ff8a71c7ea390ae8ed5fbd4f95
NimmiShashikala60 <nimmishashikala385@gmail.com>
2023-01-23 22:46:02 +0530

    new update

--- 09_Spring_Web_MVC/src/lk/ijse/spring/config/WebRootConfig.java
+++ 09_Spring_Web_MVC/src/lk/ijse/spring/config/WebRootConfig.java
@@ -6,8 +6,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@EnableWebMvc
-@ComponentScan(basePackages = "lk.ijse.spring.pojo")
 public class WebRootConfig {
     public WebRootConfig(){
         System.out.println("WebRootConfig:Instantiated");
--- 09_Spring_Web_MVC/src/lk/ijse/spring/controller/CustomerController.java
+++ 09_Spring_Web_MVC/src/lk/ijse/spring/controller/CustomerController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
+    public CustomerController(){
+        System.out.println("CustomerController");
+    }
     @GetMapping
     public void callMe(){
         System.out.println("Call Me Method Invoked By The Web Browser");
