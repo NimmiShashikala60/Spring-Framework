fd37598c9c7870f4b96cd1f16f60b37adae9dfe8
NimmiShashikala60 <nimmishashikala385@gmail.com>
2023-01-23 14:05:51 +0530

    new update

--- 08_Spring_Enviroment/src/lk/ijse/spring/pojo/DBConnection.java
+++ 08_Spring_Enviroment/src/lk/ijse/spring/pojo/DBConnection.java
@@ -14,8 +14,12 @@ public class DBConnection implements InitializingBean {
 
     @Value("${db.user.name}")
     private String dbUserName;
+
     @Value("${db.user.password}")
     private String dbPassword;
+
+    @Value("${user.email}")
+    private String userEmail;
 
     public DBConnection(){
         System.out.println("DBConnection:Instantiated");
@@ -28,5 +32,6 @@ public class DBConnection implements InitializingBean {
         System.out.println("===============");
         System.out.println(dbUserName);
         System.out.println(dbPassword);
+        System.out.println(userEmail);
     }
 }
