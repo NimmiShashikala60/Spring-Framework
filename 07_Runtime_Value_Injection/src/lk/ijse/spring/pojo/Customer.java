4cad8c57181e29f7b9fa392ac2bb98f4449e5267
NimmiShashikala60 <nimmishashikala385@gmail.com>
2023-01-23 11:57:54 +0530

    new update

--- 07_Runtime_Value_Injection/src/lk/ijse/spring/pojo/Customer.java
+++ 07_Runtime_Value_Injection/src/lk/ijse/spring/pojo/Customer.java
@@ -1,5 +1,6 @@
 package lk.ijse.spring.pojo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -14,11 +15,18 @@ public class Customer {
 //        System.out.println("Customer:Instantiated" + name);
 //    }
 
-    public Customer(@Value("Dasun,Kasun,Hansi") String names[]){
+    @Autowired(required = false)
+    public Customer(@Value("Dasun,Kasun,Hansi") String names[]) {
         System.out.println("Customer:Instantiated" + names);
-        for (String name: names){
+        for (String name : names) {
             System.out.println(name);
         }
+    }
+
+    @Autowired(required = false)
+    public Customer(@Value("Dasun") String name,@Value("10") int i) {
+        System.out.println("Customer:Instantiated:" + name);
+
     }
 }
 
