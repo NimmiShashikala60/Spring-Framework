a91c8a783ac254bed49630763c5c397cea7feeff
NimmiShashikala60 <nimmishashikala385@gmail.com>
2023-01-23 09:59:32 +0530

    new update

--- 06_Spring_Configuration/src/lk/ijse/spring/config/AppConfig.java
+++ 06_Spring_Configuration/src/lk/ijse/spring/config/AppConfig.java
@@ -3,7 +3,6 @@ package lk.ijse.spring.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.ImportResource;
 
 @Configuration
 @ComponentScan(basePackages = "lk.ijse.spring.pojo")
