4119b4a717df987297ac9d1e2b31bef98410c544
NimmiShashikala60 <nimmishashikala385@gmail.com>
2023-01-30 16:09:33 +0530

    new update

--- 10_Controller/src/lk/ijse/spring/controller/CustomerController.java
+++ 10_Controller/src/lk/ijse/spring/controller/CustomerController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
 @RequestMapping("/customer")
 public class CustomerController {
     public CustomerController(){
