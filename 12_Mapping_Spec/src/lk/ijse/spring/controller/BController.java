59c320236fecfff0b3114d0b7d93ec8f3ebf7ccb
NimmiShashikala60 <nimmishashikala385@gmail.com>
2023-01-31 15:42:00 +0530

    new  update

--- 12_Mapping_Spec/src/lk/ijse/spring/controller/BController.java
+++ 12_Mapping_Spec/src/lk/ijse/spring/controller/BController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/three")
-public class ThreeController {
+public class BController {
 
 
 //    @GetMapping("/id/{C001}/{Dasun}")
@@ -12,8 +12,14 @@ public class ThreeController {
 //        System.out.println(id+" "+name);
 //        return "Post Method Invoked";
 //    }
-    @GetMapping("/id/{id}/{name}")
+    @GetMapping("/id/{id:[C]{1}[0-9]{3}}/{name:[A-Z]{4}}")
     public String testOne12(@PathVariable String id,@PathVariable String name){
-        return "Post Method Invoked 2"+id+" "+name;
+        return "Post Method 1 Invoked 2"+id+" "+name;
+    }
+
+    @GetMapping("/id/{id:[B]{2}[0-9]{3}}/{name:[a-z]{5}}")
+    public String testOne123(@PathVariable String id,@PathVariable String name){
+        return "Post Method 1 Invoked 2"+id+" "+name;
     }
 }
+
