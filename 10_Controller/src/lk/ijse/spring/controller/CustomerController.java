28468cebf1dc4784cf14d2044321dadbc7ba6458
NimmiShashikala60 <nimmishashikala385@gmail.com>
2023-02-07 20:49:29 +0530

    new update

--- 10_Controller/src/lk/ijse/spring/controller/CustomerController.java
+++ 10_Controller/src/lk/ijse/spring/controller/CustomerController.java
@@ -4,14 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
     @GetMapping
-    public String test(){
-        return "Hello Spring";
+    public ModelAndView test(){
+        return new ModelAndView("/customer");
 
     }
 }
---
+++ 10_Controller/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: acer
+  Date: 2/7/2023
+  Time: 8:18 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>index page for Spring Web MVC</h1>
+</body>
+</html>
